9d603daf609996bf128e75ed34ae53dbcb00ca69
Heckfy <rr.akhmetoff@gmail.com>
2022-05-14 07:33:19 +0000

    ДЗ 1.2 Система типов TypeScript. ООП. SOLID

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module',
+		'project': 'tsconfig.json',
+		'tsconfigRootDir': '.'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			2
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
